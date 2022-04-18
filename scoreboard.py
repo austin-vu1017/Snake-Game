673cb64b2781568d5711e75bf66a9d0d3e18cef9
Austin <21083126+austin-vu1017@users.noreply.github.com>
2022-04-18 04:09:26 -0500

    Update scoreboard.py and added data.txt
    Added open/read/write feature to scoreboard.py so high scores from previous games. This allows high scores to be carried over to future Snake games.

--- scoreboard.py
+++ scoreboard.py
@@ -4,7 +4,8 @@ class Scoreboard(Turtle):
     def __init__(self) -> None:
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt", mode="r") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.penup()
         self.sety(260)
@@ -19,6 +20,8 @@ class Scoreboard(Turtle):
         if self.score > self.high_score:
             self.high_score = self.score
         self.score = 0
+        with open("data.txt", mode="w") as file:
+            file.write(f"{self.high_score}")
         self.update()
         
     def increase_score(self):
